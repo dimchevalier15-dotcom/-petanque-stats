b0ff56e90005f4f8ef8ab0b9aca21ff1f0cbca8e
Dimitri Chevalier <dimchevalier15@gmail.com>
2026-08-10 15:52:37 +0200

    feat: allowed host toto

--- mobile/vite.config.ts
+++ mobile/vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
     port: 5173,
+
+    allowedHosts: [
+      'petanque.liegermaster.fr',
+    ],
+
     proxy: {
       '/api': {
-        target: 'http://petanque.liegermaster.fr',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       },
     },
